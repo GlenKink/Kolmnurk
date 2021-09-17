17f7199f3beaa182eb0d0b30ffe1f2414f56e218
Glen Kink <89788372+GlenKink@users.noreply.github.com>
2021-09-17 17:51:52 +0300

    Add files via upload

---
+++ kolmnurga_P.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    float ax, ay, bx, by, cx, cy, A, B, C, ümbermõõt;
+    printf("Arvutame kolmnurga ümbermõõdu kasutades X-ja Y-koordinaate.\n");
+       
+        printf("Sisesta kolmnurga punkt A X-koordinaat: ");
+        scanf("%f", &ax);
+     
+        printf("Sisesta kolmnurga punkt A Y-koordinaat: ");
+        scanf("%f", &ay);
+     
+        printf("Sisesta kolmnurga punkt B X-koordinaat: ");
+        scanf("%f", &bx);
+     
+        printf("Sisesta kolmnurga punkt B Y-koordinaat: ");
+        scanf("%f", &by);
+     
+        printf("Sisesta kolmnurga punkt C X-koordinaat: ");
+        scanf("%f", &cx);
+     
+        printf("Sisesta kolmnurga punkt C Y-koordinaat: ");
+        scanf("%f", &cy);
+     
+    
+    
+    A = sqrt((double)(bx-ax) * (bx-ax) + (by-ay) * (by-ay));
+    B = sqrt((double)(bx-cx) * (bx-cx) + (by-cy) * (by-cy));
+    C = sqrt((double)(ax-cx) * (ax-cx) + (ay-cy) * (ay-cy));
+    
+    if(A <= 0 || B <= 0 || C <= 0){
+            printf("Küljepikkus ei tohi olla negatiivne või null.\n");
+            return 0;
+        }
+    
+    if(A < (B+C) && B < (A+C) && C < (B+A))
+        {
+        ümbermõõt = A + B + C;
+        
+            printf("\nKolmnurga ümbermõõt on: %.4f\n", ümbermõõt);
+        
+       }
+       else
+       {
+           printf("Pole võimalik kolmnurka luua!!");
+       }
+   }
+
