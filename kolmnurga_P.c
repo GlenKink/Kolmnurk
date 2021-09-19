4dd9619bd41e4f1809af945e544816a78231ec83
Glen Kink <89788372+GlenKink@users.noreply.github.com>
2021-09-19 20:55:18 +0300

    Update kolmnurga_P.c

--- kolmnurga_P.c
+++ kolmnurga_P.c
@@ -3,9 +3,9 @@
 
 int main(void){
     float ax, ay, bx, by, cx, cy, A, B, C, ümbermõõt;
-    printf("Arvutame kolmnurga ümbermõõdu kasutades X-ja Y-koordinaate.\n");
+    printf("Arvutame kolmnurga ümbermõõdu kasutades X-ja Y-koordinaate.\n"); // Anname kasutajale teada mida programm teeb
        
-        printf("Sisesta kolmnurga punkt A X-koordinaat: ");
+        printf("Sisesta kolmnurga punkt A X-koordinaat: "); // Palume kasutajal sisestada koordinaadid
         scanf("%f", &ax);
      
         printf("Sisesta kolmnurga punkt A Y-koordinaat: ");
@@ -25,16 +25,16 @@ int main(void){
      
     
     
-    A = sqrt((bx-ax) * (bx-ax) + (by-ay) * (by-ay));
+    A = sqrt((bx-ax) * (bx-ax) + (by-ay) * (by-ay)); // Kasutame vektori pikkuse leidmise valemit
     B = sqrt((bx-cx) * (bx-cx) + (by-cy) * (by-cy));
     C = sqrt((ax-cx) * (ax-cx) + (ay-cy) * (ay-cy));
     
-    if(A <= 0 || B <= 0 || C <= 0){
+    if(A <= 0 || B <= 0 || C <= 0){  // Väikene tsükkel, mis ei lase küljepikkusel olla negatiivne või null
             printf("Küljepikkus ei tohi olla negatiivne või null.\n");
             return 0;
         }
     
-    if(A < (B+C) && B < (A+C) && C < (B+A))
+    if(A < (B+C) && B < (A+C) && C < (B+A)) // Tsükkel, mis teeb kindlaks, et kolmnurk moodustub
         {
         ümbermõõt = A + B + C;
         
